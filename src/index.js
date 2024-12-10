feb713037aa46f9f3e7b1dbc41e41cb34687c3c5
Maria Ordóñez <77161167+catliar666@users.noreply.github.com>
2024-12-10 13:57:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use('/api/v1/workouts' , v1Router)
-app.listen(PORT ,'::', () => {
+app.listen(PORT , () => {
     console.log(`API i listening on port ${PORT}`)
 })
